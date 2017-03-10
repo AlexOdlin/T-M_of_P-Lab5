af704f55f38a11cbc948cb9f58e9ec3acd7a783e
AlexOdlin <rockguitarist94alex@gmail.com>
2017-03-10 11:53:39 +0300

    Update bar.cpp

--- bar.cpp
+++ bar.cpp
@@ -1,12 +1,12 @@
 #include "catch.hpp"
 
-int bar(int a,int b){
+int sum(int a,int b){
 
   return a+b;
 }
 
-TEST_CASE("sum","[bar]"){
-  REQUIRE( bar(1,2) == 3 );
-  REQUIRE( bar(2,3) == 4 );
+TEST_CASE("sum","[sum]"){
+  REQUIRE( sum(1,2) == 3 );
+  REQUIRE( sum(2,3) == 4 );
   
 }
